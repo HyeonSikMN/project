dd95b00271e81d137371329c5a6b02887eedd7da
HyeonSikMN <138096145+HyeonSikMN@users.noreply.github.com>
2023-07-03 16:15:45 +0900

    commit

--- Project_pet/src/main/java/com/korea/pet/HomeController.java
+++ Project_pet/src/main/java/com/korea/pet/HomeController.java
@@ -1,11 +1,16 @@
 package com.korea.pet;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 import dao.HomeDAO;
+import vo.HomeVO;
 
 @Controller
 public class HomeController {
@@ -13,10 +18,19 @@ public class HomeController {
 	@Autowired	//자동주입
 	HttpServletRequest request;
 	
-	public static final String PATH = "/WEB-INF/views/home/";
+	public static final String PATH = "/WEB-INF/views/";
 	
 	HomeDAO home_dao;
 	public void setHome_dao(HomeDAO home_dao) {
 		this.home_dao = home_dao;
+	}
+	
+	@RequestMapping(value= {"/","home.do"})
+	public String selectList(Model model) {
+		List<HomeVO> list = home_dao.selectAll();
+		
+		model.addAttribute("list", list);
+		return PATH + "home.jsp";
+	
 	}
 }
